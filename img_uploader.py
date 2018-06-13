361ed72978c072fd63ec685ddcd2e5c2d9c2ddfa
Kike-Ramirez <info@kikeramirez.org>
2018-06-13 18:03:30 +0100

    Upload to Pictures folder with universal access. Get URL link as result.

--- img_uploader.py
+++ img_uploader.py
@@ -15,16 +15,32 @@ if not creds or creds.invalid:
     creds = tools.run_flow(flow, store)
 DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
 
-FILES = (
-    ('Pictures/foto1.jpg', 'image/jpeg'),
-    ('Pictures/foto2.jpg', 'image/jpeg'),
-)
+# Aquí definimos los parámetros del archivo a subir, carpeta y enlace. 
+# JAVO, deberías linkar con tu script de TD las variables localname, servername y URL_drive.
 
-for filename, mimeType in FILES:
-    metadata = {'name': filename}
-    if mimeType:
-        metadata['mimeType'] = mimeType
-    res = DRIVE.files().create(body=metadata, media_body=filename).execute()
-    if res:
-        print('Uploaded "%s" (%s)' % (filename, res['mimeType']))
+# Ruta del archivo local
+localname = 'Pictures/foto1.jpg'
+
+# Nombre del archivo en el servidor
+servername = 'sonarabsolute2018.jpg'
+
+# Tipo del archivo -MIME TYPE-
+mimeType = 'image/jpeg'
+
+# Id de la carpeta "Pictures" creada en Google Drive.
+folder = ['1MQE1O81aZgC8k2Nxg_i9RSOksJZsQigB']
+
+metadata = {'name': servername, 'mimeType': mimeType, 'parents': folder}
+
+# Subimos el archivo y recibimos la respuesta
+res = DRIVE.files().create(body=metadata, media_body=localname, fields='id, webViewLink').execute()
+
+if res:
+    print('Subido "%s" con id: %s' % (localname, res['id']))
+    URL_drive = res['webViewLink']
+
+
+# En la variable URL tienes el enlace para verlo
+print('URL Link: %s' % URL_drive)
+
 
